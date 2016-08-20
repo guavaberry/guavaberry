15e83e533b42dde87584966a2c3f33812379df70
Filippo Squillace <squillace.filippo@gmail.com>
2016-08-20 16:06:02 +0100

    Issue #3: Add more gpg skips

--- util/deploy.sh
+++ util/deploy.sh
@@ -8,7 +8,7 @@ if [ "$TRAVIS_BRANCH" == "dev" ] || \
    [ "$TRAVIS_BRANCH" == "master" ]; then
   echo "Uploading Guavaberry snapshot to OSS..."
 
-  mvn clean deploy --settings="util/deploy-settings.xml" -DskipTests=true
+  mvn clean deploy --settings="util/deploy-settings.xml" -DskipTests=true -Dgpg.skip=true
 
   echo "Guavaberry snapshot uploaded to OSS."
 fi

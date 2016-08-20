3d61b05abc5ecd45593b3716295eb266037e3465
Filippo Squillace <squillace.filippo@gmail.com>
2016-08-20 19:29:07 +0100

    Issue #3: Use encrypt-file

--- util/deploy.sh
+++ util/deploy.sh
@@ -14,9 +14,7 @@ if [ "$TRAVIS_BRANCH" == "dev" ]; then
 elif [ "$TRAVIS_BRANCH" == "master" ]; then
   echo "Uploading Guavaberry release to OSS..."
 
-  openssl aes-256-cbc -K $encrypted_bbc983d8127a_key -iv $encrypted_bbc983d8127a_iv -in util/pubring.gpg.enc -out util/pubring.gpg -d
-  openssl aes-256-cbc -K $encrypted_bbc983d8127a_key -iv $encrypted_bbc983d8127a_iv -in util/secring.gpg.enc -out util/secring.gpg -d
-
+  # Check the key files
   ls -la util/
 
   # If branch master deploy a release instead of a snapshot

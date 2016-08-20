593e822e216c169d91a46c1b1c0e442f2df886b5
Filippo Squillace <squillace.filippo@gmail.com>
2016-08-20 19:20:35 +0100

    Issue #3: Use encrypt-file from travis

--- util/deploy.sh
+++ util/deploy.sh
@@ -14,15 +14,15 @@ if [ "$TRAVIS_BRANCH" == "dev" ]; then
 elif [ "$TRAVIS_BRANCH" == "master" ]; then
   echo "Uploading Guavaberry release to OSS..."
 
-  openssl aes-256-cbc -pass pass:$ENCRYPTION_PASSWORD -in util/pubring.gpg.enc -out util/pubring.gpg -d
-  openssl aes-256-cbc -pass pass:$ENCRYPTION_PASSWORD -in util/secring.gpg.enc -out util/secring.gpg -d
+  openssl aes-256-cbc -K $encrypted_bbc983d8127a_key -iv $encrypted_bbc983d8127a_iv -in util/pubring.gpg.enc -out util/pubring.gpg -d
+  openssl aes-256-cbc -K $encrypted_bbc983d8127a_key -iv $encrypted_bbc983d8127a_iv -in util/secring.gpg.enc -out util/secring.gpg -d
 
   ls -la util/
 
   # If branch master deploy a release instead of a snapshot
   sed -ie "s/-SNAPSHOT//" pom.xml
 
-  mvn clean deploy --settings="util/deploy-settings.xml" -DskipTests=true -DperformRelease=true
+  mvn clean deploy --settings="util/deploy-settings.xml" -DskipTests=true -DperformRelease=true -Dgpg.passphrase=${GPG_PASSPHRASE} -Dgpg.keyname=${GPG_KEYNAME}
 
   echo "Guavaberry release uploaded to OSS."
 fi
